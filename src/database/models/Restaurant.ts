4b34993b528c961b20812393ee82131ca5f4654c
manuelaperezsimon <manuela@swipoo.com>
2024-01-22 22:50:16 +0100

    create the Restaurant Schema

---
+++ src/database/models/Restaurant.ts
@@ -0,0 +1,42 @@
+import { model, Schema } from "mongoose";
+
+const restaurantSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  createAt: {
+    type: Date,
+    default: new Date(),
+  },
+  restaurantHours: [
+    {
+      open: {
+        type: String,
+        required: true,
+      },
+      close: {
+        type: String,
+        required: true,
+      },
+    },
+    {
+      open: {
+        type: String,
+      },
+      close: {
+        type: String,
+      },
+    },
+  ],
+  status: { Boolean },
+  tags: { Array },
+});
+
+const Restaurant = model("Restaurant", restaurantSchema, "restaurantes");
+
+export default Restaurant;
